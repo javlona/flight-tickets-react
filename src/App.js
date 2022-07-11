abee66d947ce82e01b4398ef330de56bff8eeef0
Javlon <59303287+javlona@users.noreply.github.com>
2022-07-11 18:19:28 -0500

    material ui

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import {Routes, Route} from 'react-router-dom';
 import './App.css';
+import Main from './components/Main';
+import Nav from './components/Nav';
 
 
 function App() {
   return (
     <div className="App">
-      
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
     </div>
   );
 }
--- src/components/BasicDatePicker/index.js
+++ src/components/BasicDatePicker/index.js
@@ -2,20 +2,20 @@ import * as React from 'react';
 import TextField from '@mui/material/TextField';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
 
-export default function BasicDatePicker() {
-  const [value, setValue] = React.useState(null);
+export default function BasicDateTimePicker() {
+  const [value, setValue] = React.useState(new Date());
 
   return (
     <LocalizationProvider dateAdapter={AdapterDateFns}>
-      <DatePicker
-        label="Basic example"
+      <DateTimePicker
+        renderInput={(props) => <TextField {...props} />}
+        label="DateTimePicker"
         value={value}
         onChange={(newValue) => {
           setValue(newValue);
         }}
-        renderInput={(params) => <TextField {...params} />}
       />
     </LocalizationProvider>
   );
